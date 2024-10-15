d3038ff00bf091d41ef8592e242fbefb8bb4154f
Robert Alexander <ralexander87@protonmail.com>
2024-10-15 16:48:23 +0200

    edit

--- nvim/lua/lualine/themes/lushwal.lua
+++ nvim/lua/lualine/themes/lushwal.lua
@@ -1,75 +1,75 @@
 return {
   command = {
     a = {
-      bg = "#7FA3C3",
-      fg = "#191D24",
+      bg = "#59619B",
+      fg = "#0C0B18",
       gui = "bold"
     },
     b = {
-      bg = "#A49984",
-      fg = "#7FA3C3"
+      bg = "#857C8D",
+      fg = "#59619B"
     }
   },
   inactive = {
     a = {
-      bg = "#A49984",
-      fg = "#EBDCBC"
+      bg = "#857C8D",
+      fg = "#BDAFCA"
     },
     b = {
-      bg = "#A49984",
-      fg = "#EBDCBC"
+      bg = "#857C8D",
+      fg = "#BDAFCA"
     },
     c = {
-      bg = "#A49984",
-      fg = "#EBDCBC"
+      bg = "#857C8D",
+      fg = "#BDAFCA"
     }
   },
   insert = {
     a = {
-      bg = "#8F768C",
-      fg = "#191D24",
+      bg = "#2B3E82",
+      fg = "#0C0B18",
       gui = "bold"
     },
     b = {
-      bg = "#A49984",
-      fg = "#8F768C"
+      bg = "#857C8D",
+      fg = "#2B3E82"
     }
   },
   normal = {
     a = {
-      bg = "#D18971",
-      fg = "#191D24",
+      bg = "#CA5B4E",
+      fg = "#0C0B18",
       gui = "bold"
     },
     b = {
-      bg = "#A49984",
-      fg = "#D18971"
+      bg = "#857C8D",
+      fg = "#CA5B4E"
     },
     c = {
-      bg = "#191D24",
-      fg = "#EBDCBC"
+      bg = "#0C0B18",
+      fg = "#BDAFCA"
     }
   },
   replace = {
     a = {
-      bg = "#A99879",
-      fg = "#191D24",
+      bg = "#324376",
+      fg = "#0C0B18",
       gui = "bold"
     },
     b = {
-      bg = "#A49984",
-      fg = "#A99879"
+      bg = "#857C8D",
+      fg = "#324376"
     }
   },
   visual = {
     a = {
-      bg = "#597497",
-      fg = "#191D24",
+      bg = "#F89472",
+      fg = "#0C0B18",
       gui = "bold"
     },
     b = {
-      bg = "#A49984",
-      fg = "#597497"
+      bg = "#857C8D",
+      fg = "#F89472"
     }
   }
 }
